d304a25c00d3e5945d8174dbd8d5f57c8edf80d5
Dreamwalker <aristojeff@gmail.com>
2022-07-03 17:24:41 +0900

    Update health_care_bottom_menu_widget.dart

---
+++ lib/ep1201_health_care_app/src/ui/health_care_bottom_menu_widget.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_21/ep1201_health_care_app/src/health_care_style.dart';
+
+class HCAMenu {
+  String? title;
+  Widget? iconWidget;
+  int? index;
+
+  HCAMenu({this.title, this.iconWidget, this.index});
+}
+
+List<HCAMenu> bottomMenuItems = [
+  HCAMenu(title: "Home", index: 0, iconWidget: Icon(Icons.home_outlined)),
+];
+
+class HealthCareBottomMenuWidget extends StatefulWidget {
+  const HealthCareBottomMenuWidget({Key? key}) : super(key: key);
+
+  @override
+  State<HealthCareBottomMenuWidget> createState() => _HealthCareBottomMenuWidgetState();
+}
+
+class _HealthCareBottomMenuWidgetState extends State<HealthCareBottomMenuWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      child: Container(
+        height: 72,
+        padding: EdgeInsets.symmetric(horizontal: 4),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                children: [
+                  Container(
+                    height: 3,
+                    color: healthCarePrimaryColor,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.bar_chart),
+                    iconSize: 32,
+                    color: healthCarePrimaryColor,
+                  ),
+                  Text(
+                    "Home",
+                    style: TextStyle(
+                      color: healthCarePrimaryColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  Container(
+                    height: 3,
+                    color: Colors.transparent,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.bar_chart),
+                    iconSize: 32,
+                    color: healthCarePrimaryColor,
+                  ),
+                  Text(
+                    "Billing",
+                    style: TextStyle(
+                      color: healthCarePrimaryColor,
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  Container(
+                    height: 3,
+                    color: Colors.transparent,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.bar_chart),
+                    iconSize: 32,
+                    color: healthCarePrimaryColor,
+                  ),
+                  Text(
+                    "Paitents",
+                    style: TextStyle(
+                      color: healthCarePrimaryColor,
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  Container(
+                    height: 3,
+                    color: Colors.transparent,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.bar_chart),
+                    iconSize: 32,
+                    color: healthCarePrimaryColor,
+                  ),
+                  Text(
+                    "Credentialing",
+                    style: TextStyle(
+                      color: healthCarePrimaryColor,
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  Container(
+                    height: 3,
+                    color: Colors.transparent,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.bar_chart),
+                    iconSize: 32,
+                    color: healthCarePrimaryColor,
+                  ),
+                  Text(
+                    "Documents",
+                    style: TextStyle(
+                      color: healthCarePrimaryColor,
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
