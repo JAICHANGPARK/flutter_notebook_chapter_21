5b5484bbd0fde58a8ac3b9509436b99057654fc6
Dreamwalker <aristojeff@gmail.com>
2022-07-03 17:07:54 +0900

    Create health_care_home_page.dart

---
+++ lib/ep1201_health_care_app/src/ui/health_care_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class HealthCareHomePage extends StatefulWidget {
+  const HealthCareHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HealthCareHomePage> createState() => _HealthCareHomePageState();
+}
+
+class _HealthCareHomePageState extends State<HealthCareHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
