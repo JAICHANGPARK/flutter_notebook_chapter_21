74f978892c925ce3e6b2fbf287f8cbeb7c6d9997
Dreamwalker <aristojeff@gmail.com>
2022-07-03 17:07:57 +0900

    Update health_care_mobile_app.dart

--- lib/ep1201_health_care_app/health_care_mobile_app.dart
+++ lib/ep1201_health_care_app/health_care_mobile_app.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_notebook_chapter_21/ep1201_health_care_app/src/ui/health_care_home_page.dart';
 
 class HealthcareMobileApp extends StatelessWidget {
   const HealthcareMobileApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      routes: {
+        "/": (context) => HealthCareHomePage(),
+      },
+    );
   }
 }
