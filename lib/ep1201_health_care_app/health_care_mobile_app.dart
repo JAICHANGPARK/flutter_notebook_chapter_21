859a32e28b85720566d582e21d4eca9de9c100da
Dreamwalker <aristojeff@gmail.com>
2022-07-03 17:06:15 +0900

    Create health_care_mobile_app.dart

---
+++ lib/ep1201_health_care_app/health_care_mobile_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class HealthcareMobileApp extends StatelessWidget {
+  const HealthcareMobileApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
