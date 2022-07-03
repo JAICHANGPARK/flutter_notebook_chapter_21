f877ae8d922569509b501eeab58974538670486a
Dreamwalker <aristojeff@gmail.com>
2022-07-03 17:07:52 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_21/ep1201_health_care_app/health_care_mobile_app.dart';
 
-
-main() {
-  runApp(app)
+void main() {
+  runApp(HealthcareMobileApp());
 }
