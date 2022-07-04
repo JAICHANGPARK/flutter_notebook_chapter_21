1b379514c068e3d2e51b2a751674698dd26b7b45
Dreamwalker <aristojeff@gmail.com>
2022-07-04 14:09:17 +0900

    Update health_care_bottom_menu_widget.dart

--- lib/ep1201_health_care_app/src/ui/health_care_bottom_menu_widget.dart
+++ lib/ep1201_health_care_app/src/ui/health_care_bottom_menu_widget.dart
@@ -11,6 +11,10 @@ class HCAMenu {
 
 List<HCAMenu> bottomMenuItems = [
   HCAMenu(title: "Home", index: 0, iconWidget: Icon(Icons.home_outlined)),
+  HCAMenu(title: "Billing", index: 1, iconWidget: Icon(Icons.card_giftcard)),
+  HCAMenu(title: "Paitents", index: 2, iconWidget: Icon(Icons.person_outlined)),
+  HCAMenu(title: "Cretendial", index: 3, iconWidget: Icon(Icons.list_alt)),
+  HCAMenu(title: "Documents", index: 4, iconWidget: Icon(Icons.document_scanner_outlined)),
 ];
 
 class HealthCareBottomMenuWidget extends StatefulWidget {
