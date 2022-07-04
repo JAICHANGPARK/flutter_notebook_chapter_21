39fc38380be4629d3a97f0bd67518a85cc8ec101
Dreamwalker <aristojeff@gmail.com>
2022-07-04 14:17:47 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_21/ep1201_health_care_app/health_care_mobile_app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(HealthcareMobileApp());
+  runApp(
+    ProviderScope(
+      child: HealthcareMobileApp(),
+    ),
+  );
 }
