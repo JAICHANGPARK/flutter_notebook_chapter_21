a05aab80de1e0d4a3ed5b538be48197309419310
Dreamwalker <aristojeff@gmail.com>
2022-07-05 18:28:08 +0900

    Update hc_chat.dart

--- lib/ep1201_health_care_app/src/model/hc_chat.dart
+++ lib/ep1201_health_care_app/src/model/hc_chat.dart
@@ -27,12 +27,31 @@ List<HcChat> hcChatItems = [
     time: "5:12 PM",
   ),
   HcChat(
-    name: "Taylor Davis",
+    name: "Charlie Donin",
     msg:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-    unreadMsg: 1,
+    unreadMsg: 2,
     profileImg: "https://cdn.pixabay.com/photo/2020/05/17/20/21/cat-5183427_960_720.jpg",
-    tag: "BILLING",
-    time: "5:12 PM",
+    tag: null,
+    time: "3:23 PM",
   ),
+  HcChat(
+    name: "Charlie Donin",
+    msg:
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    unreadMsg: 0,
+    profileImg: "https://cdn.pixabay.com/photo/2020/10/04/10/43/horse-5625922_960_720.jpg",
+    tag: "CRED",
+    time: "3:23 PM",
+  ),
+  HcChat(
+    name: "Charlie Donin",
+    msg:
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    unreadMsg: 0,
+    profileImg: "https://cdn.pixabay.com/photo/2020/05/17/20/21/cat-5183427_960_720.jpg",
+    tag: BILLING,
+    time: "3:23 PM",
+  ),
+
 ];
