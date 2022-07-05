cc7500d020bf35ced3a31f89e7e8082a2c3265d1
Dreamwalker <aristojeff@gmail.com>
2022-07-05 18:24:12 +0900

    Create hc_chat.dart

---
+++ lib/ep1201_health_care_app/src/model/hc_chat.dart
@@ -0,0 +1,11 @@
+class HcChat {
+
+  String? name;
+  String? msg;
+  String? tag;
+  String? time;
+  int? unreadMsg;
+  String profileImg;
+
+  HcChat(this.name, this.msg, this.tag, this.time, this.unreadMsg, this.profileImg);
+}
