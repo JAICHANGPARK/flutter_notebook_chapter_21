faec2973ab39bb778da0d91a9c3a14d6d375a0e3
Dreamwalker <aristojeff@gmail.com>
2022-07-06 16:15:19 +0900

    Create hc_chat_msg_page.dart

---
+++ lib/ep1201_health_care_app/src/ui/hc_chat_msg_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HcChatMsgPage extends StatefulWidget {
+  const HcChatMsgPage({Key? key}) : super(key: key);
+
+  @override
+  State<HcChatMsgPage> createState() => _HcChatMsgPageState();
+}
+
+class _HcChatMsgPageState extends State<HcChatMsgPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
