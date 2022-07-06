bf2369cd1e5611c1e2147ba83dfcced9873a1cef
Dreamwalker <aristojeff@gmail.com>
2022-07-06 16:20:34 +0900

    Update hc_chat_msg_page.dart

--- lib/ep1201_health_care_app/src/ui/hc_chat_msg_page.dart
+++ lib/ep1201_health_care_app/src/ui/hc_chat_msg_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_21/ep1201_health_care_app/src/riverpod/select_chat_msg.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HcChatMsgPage extends StatefulWidget {
   const HcChatMsgPage({Key? key}) : super(key: key);
@@ -10,6 +12,21 @@ class HcChatMsgPage extends StatefulWidget {
 class _HcChatMsgPageState extends State<HcChatMsgPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Consumer(builder: (context, ref, _) {
+      final item = ref.watch(selectedChatMsgItem);
+      return Column(
+        children: [
+          Text("I'm having a problem on a clame #12345"),
+          Row(
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(item?.profileImg ?? ""),
+              ),
+
+            ],
+          )
+        ],
+      );
+    });
   }
 }
