aefb187d401a73504fd7b91c3e53a83ad8b575ba
Dreamwalker <aristojeff@gmail.com>
2022-07-06 16:20:31 +0900

    Create select_chat_msg.dart

---
+++ lib/ep1201_health_care_app/src/riverpod/select_chat_msg.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_notebook_chapter_21/ep1201_health_care_app/src/model/hc_chat.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final selectedChatMsgItem = StateProvider<HcChat?>((ref) => null);
