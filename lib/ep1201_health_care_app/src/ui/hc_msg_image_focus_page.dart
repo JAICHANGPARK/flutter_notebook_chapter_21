ae5071cfc003eafec9c743665020d0c24194632a
Dreamwalker <aristojeff@gmail.com>
2022-07-06 16:33:45 +0900

    Update hc_msg_image_focus_page.dart

--- lib/ep1201_health_care_app/src/ui/hc_msg_image_focus_page.dart
+++ lib/ep1201_health_care_app/src/ui/hc_msg_image_focus_page.dart
@@ -2,10 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_21/ep1201_health_care_app/src/riverpod/select_chat_msg.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-
-
 class HcMsgImageFocusPage extends ConsumerWidget {
   final String? img;
+
   const HcMsgImageFocusPage({Key? key, this.img}) : super(key: key);
 
   @override
@@ -14,7 +13,12 @@ class HcMsgImageFocusPage extends ConsumerWidget {
     return Scaffold(
       body: Hero(
         tag: "img_${item?.name}",
-        child: Center(child: Image.network(img ?? "", fit: BoxFit.cover,)),
+        child: Center(
+          child: Image.network(
+            img ?? "",
+            fit: BoxFit.cover,
+          ),
+        ),
       ),
     );
   }
