7b6d67cd60b24207714c21607fda4a74aa48fdf2
Dreamwalker <aristojeff@gmail.com>
2022-07-07 14:41:09 +0900

    Update hc_home_widget.dart

--- lib/ep1201_health_care_app/src/ui/hc_home_widget.dart
+++ lib/ep1201_health_care_app/src/ui/hc_home_widget.dart
@@ -27,6 +27,22 @@ class HcHomeWidget extends StatelessWidget {
               margin: EdgeInsets.symmetric(vertical: 16),
               height: 240,
               color: Colors.blue,
+            ),
+            Container(
+              height: 36,
+              width: 260,
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(24)
+              ),
+              child: Row(
+                children: [
+                  TextButton(onPressed: (){}, child: Text("1m")),
+                  TextButton(onPressed: (){}, child: Text("1y")),
+                  TextButton(onPressed: (){}, child: Text("MTD")),
+                  TextButton(onPressed: (){}, child: Text("All")),
+                ],
+              ),
             )
           ],
         ),
