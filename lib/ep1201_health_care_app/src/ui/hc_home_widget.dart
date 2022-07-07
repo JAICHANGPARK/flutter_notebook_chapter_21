3a4f4bf9a11bd2bfbaf6df68f2ef3080e74b6a60
Dreamwalker <aristojeff@gmail.com>
2022-07-07 14:38:57 +0900

    Update hc_home_widget.dart

--- lib/ep1201_health_care_app/src/ui/hc_home_widget.dart
+++ lib/ep1201_health_care_app/src/ui/hc_home_widget.dart
@@ -1,11 +1,36 @@
 import 'package:flutter/material.dart';
 
-
 class HcHomeWidget extends StatelessWidget {
   const HcHomeWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Net Collections"),
+                Text(
+                  "\$35,543",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28,
+                  ),
+                )
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 16),
+              height: 240,
+              color: Colors.blue,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
