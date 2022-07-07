90ad6ef5ccd3453694cb4b551a6865582314d598
Dreamwalker <aristojeff@gmail.com>
2022-07-07 14:33:24 +0900

    Create hc_home_widget.dart

---
+++ lib/ep1201_health_care_app/src/ui/hc_home_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class HcHomeWidget extends StatelessWidget {
+  const HcHomeWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
