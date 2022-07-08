bf8e681ea044a42942ecee6f4ba3887d590dda43
Dreamwalker <aristojeff@gmail.com>
2022-07-08 16:36:28 +0900

    Create looksie.seller.home.page.dart

---
+++ lib/ep1206_looksie_seller_app/src/ui/looksie.seller.home.page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LooksieSellerHomePage extends StatefulWidget {
+  const LooksieSellerHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<LooksieSellerHomePage> createState() => _LooksieSellerHomePageState();
+}
+
+class _LooksieSellerHomePageState extends State<LooksieSellerHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
