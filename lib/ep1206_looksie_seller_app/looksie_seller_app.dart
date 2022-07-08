7e2a824406c3d4de5f5a782ac82e277300a9b4c0
Dreamwalker <aristojeff@gmail.com>
2022-07-08 16:35:27 +0900

    Create looksie_seller_app.dart

---
+++ lib/ep1206_looksie_seller_app/looksie_seller_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class LooksieSellerApp extends StatelessWidget {
+  const LooksieSellerApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
