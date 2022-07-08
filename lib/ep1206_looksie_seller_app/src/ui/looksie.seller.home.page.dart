290f3e00533512b0d0a2e357e5f5796aeb60be96
Dreamwalker <aristojeff@gmail.com>
2022-07-08 16:39:04 +0900

    Update looksie.seller.home.page.dart

--- lib/ep1206_looksie_seller_app/src/ui/looksie.seller.home.page.dart
+++ lib/ep1206_looksie_seller_app/src/ui/looksie.seller.home.page.dart
@@ -11,21 +11,23 @@ class _LooksieSellerHomePageState extends State<LooksieSellerHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
         items: [
           BottomNavigationBarItem(
             icon: Icon(
               Icons.home_filled,
             ),
-            label: "Home"
+            label: ""
           ),
           BottomNavigationBarItem(
               icon: Icon(
                 Icons.person_outline,
               ),
-              label: "Home"
+              label: ""
           ),
           BottomNavigationBarItem(
               icon: Icon(
@@ -37,7 +39,13 @@ class _LooksieSellerHomePageState extends State<LooksieSellerHomePage> {
               icon: Icon(
                 Icons.chat_bubble_outline_outlined,
               ),
-              label: "Home"
+              label: ""
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.settings_outlined,
+              ),
+              label: ""
           )
         ],
       ),
