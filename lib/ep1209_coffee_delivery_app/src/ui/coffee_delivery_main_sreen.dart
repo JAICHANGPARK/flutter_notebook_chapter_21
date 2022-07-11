fa6bf6a6da8b17c301565b5c180ac55a1ec3af7e
Dreamwalker <aristojeff@gmail.com>
2022-07-11 15:38:19 +0900

    Update coffee_delivery_main_sreen.dart

---
+++ lib/ep1209_coffee_delivery_app/src/ui/coffee_delivery_main_sreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class CoffeeDeliveryMainScreen extends StatefulWidget {
+  const CoffeeDeliveryMainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CoffeeDeliveryMainScreen> createState() => _CoffeeDeliveryMainScreenState();
+}
+
+class _CoffeeDeliveryMainScreenState extends State<CoffeeDeliveryMainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
