6e72b76496b63695577511cd5673b17ca2b105d5
Dreamwalker <aristojeff@gmail.com>
2022-07-11 15:38:15 +0900

    Update coffee_delivery_app.dart

--- lib/ep1209_coffee_delivery_app/coffee_delivery_app.dart
+++ lib/ep1209_coffee_delivery_app/coffee_delivery_app.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_21/ep1209_coffee_delivery_app/src/ui/coffee_delivery_main_sreen.dart';
 
 class CoffeeDeliveryApp extends StatelessWidget {
   const CoffeeDeliveryApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      routes: {
+        "/": (context) => CoffeeDeliveryMainScreen(),
+      },
+    );
   }
 }
