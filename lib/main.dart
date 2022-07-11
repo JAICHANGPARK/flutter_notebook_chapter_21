3feca247b93d0ae822c5c825406451d4450dd306
Dreamwalker <aristojeff@gmail.com>
2022-07-11 15:37:26 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_chapter_21/ep1206_looksie_seller_app/looksie_seller_app.dart';
+import 'package:flutter_notebook_chapter_21/ep1209_coffee_delivery_app/coffee_delivery_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(
     ProviderScope(
-      child: LooksieSellerApp(),
+      child: CoffeeDeliveryApp(),
     ),
   );
 }
