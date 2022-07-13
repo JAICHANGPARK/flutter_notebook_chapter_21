43e1d9ed374577c2ab0fc7dd2336f4aa77e12c76
Dreamwalker <aristojeff@gmail.com>
2022-07-13 10:29:45 +0900

    Update coffee.menu.page.dart

--- lib/ep1209_coffee_delivery_app/src/ui/coffee.menu.page.dart
+++ lib/ep1209_coffee_delivery_app/src/ui/coffee.menu.page.dart
@@ -11,9 +11,13 @@ class _CoffeeMenuPageState extends State<CoffeeMenuPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text("Menu"),
-
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 0,
       ),
       body: SafeArea(
         child: Stack(),
