116405251458aaa4946a5a7765958dec0eb68561
Dreamwalker <aristojeff@gmail.com>
2022-07-13 10:26:59 +0900

    Create coffee.menu.page.dart

---
+++ lib/ep1209_coffee_delivery_app/src/ui/coffee.menu.page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class CoffeeMenuPage extends StatefulWidget {
+  const CoffeeMenuPage({Key? key}) : super(key: key);
+
+  @override
+  State<CoffeeMenuPage> createState() => _CoffeeMenuPageState();
+}
+
+class _CoffeeMenuPageState extends State<CoffeeMenuPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
