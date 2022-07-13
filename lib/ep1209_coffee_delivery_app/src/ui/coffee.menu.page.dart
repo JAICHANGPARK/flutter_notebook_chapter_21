0b5a854518ba7dae3f7e5a5e7725672aa818b20c
Dreamwalker <aristojeff@gmail.com>
2022-07-13 10:32:11 +0900

    Update coffee.menu.page.dart

--- lib/ep1209_coffee_delivery_app/src/ui/coffee.menu.page.dart
+++ lib/ep1209_coffee_delivery_app/src/ui/coffee.menu.page.dart
@@ -20,7 +20,55 @@ class _CoffeeMenuPageState extends State<CoffeeMenuPage> {
         elevation: 0,
       ),
       body: SafeArea(
-        child: Stack(),
+        child: Stack(
+          children: [
+            Positioned(
+              left: 16,
+              right: 16,
+              bottom: 0,
+              top: 0,
+              child: Column(
+                children: [],
+              ),
+            ),
+            Positioned(
+              right: 16,
+              bottom: 32,
+              child: SizedBox(
+                height: 58,
+                width: 58,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 0,
+                      right: 0,
+                      top: 0,
+                      bottom: 0,
+                      child: CircleAvatar(
+                        backgroundColor: Colors.red[200],
+                        child: Icon(Icons.shopping_bag_outlined),
+                        foregroundColor: Colors.black,
+                      ),
+                    ),
+                    Positioned(
+                      right: 0,
+                      top: 0,
+                      child: CircleAvatar(
+                        radius: 10,
+                        backgroundColor: Colors.black,
+                        child: Text(
+                          "3",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        foregroundColor: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
