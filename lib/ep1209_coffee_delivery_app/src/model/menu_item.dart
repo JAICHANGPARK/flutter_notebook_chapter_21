d3deee85f6bcccacce963186b2c86e9c5dee205b
Dreamwalker <aristojeff@gmail.com>
2022-07-14 10:11:57 +0900

    Update menu_item.dart

--- lib/ep1209_coffee_delivery_app/src/model/menu_item.dart
+++ lib/ep1209_coffee_delivery_app/src/model/menu_item.dart
@@ -30,5 +30,25 @@ List<CoffeeMenuItem> coffeeMenuItems = [
     title: "Kopi Susu Brutal",
     price: 2.05,
     img: "https://cdn.pixabay.com/photo/2017/01/12/14/22/coffee-1974841__340.jpg",
+  ),
+  CoffeeMenuItem(
+    title: "Kopi Susu Brutal",
+    price: 2.05,
+    img: "https://cdn.pixabay.com/photo/2017/01/12/14/22/coffee-1974841__340.jpg",
+  ),
+  CoffeeMenuItem(
+    title: "Caramel Santuy",
+    price: 2.95,
+    img: "https://cdn.pixabay.com/photo/2014/12/11/02/57/coffee-563800__340.jpg",
+  ),
+  CoffeeMenuItem(
+    title: "Cappucino Italiano",
+    price: 2.15,
+    img: "https://cdn.pixabay.com/photo/2016/11/29/02/10/caffeine-1866758__340.jpg",
+  ),
+  CoffeeMenuItem(
+    title: "Kopi Susu Brutal",
+    price: 2.05,
+    img: "https://cdn.pixabay.com/photo/2017/01/12/14/22/coffee-1974841__340.jpg",
   )
 ];
