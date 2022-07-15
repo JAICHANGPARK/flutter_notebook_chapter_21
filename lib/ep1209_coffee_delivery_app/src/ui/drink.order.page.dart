1428eedaa2b2b7c279300a067caa2c8faf794058
Dreamwalker <aristojeff@gmail.com>
2022-07-15 09:03:55 +0900

    Update drink.order.page.dart

--- lib/ep1209_coffee_delivery_app/src/ui/drink.order.page.dart
+++ lib/ep1209_coffee_delivery_app/src/ui/drink.order.page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class DrinkOrderPage extends StatefulWidget {
-  const DrinkOrderPage({Key? key}) : super(key: key);
+  final MenuItem? menuItem;
+  const DrinkOrderPage({Key? key, this.menuItem}) : super(key: key);
 
   @override
   State<DrinkOrderPage> createState() => _DrinkOrderPageState();
@@ -11,11 +12,18 @@ class _DrinkOrderPageState extends State<DrinkOrderPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
         elevation: 0,
       ),
+      body: SafeArea(
+          child: Stack(
+        children: [
+          Positioned(child: Container()),
+        ],
+      )),
     );
   }
 }
