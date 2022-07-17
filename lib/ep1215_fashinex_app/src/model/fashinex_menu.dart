8410cdd78ceb083b76201a708ce730ce11a860c2
Dreamwalker <aristojeff@gmail.com>
2022-07-17 09:44:49 +0900

    Create fashinex_menu.dart

---
+++ lib/ep1215_fashinex_app/src/model/fashinex_menu.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class FashinexMenu {
+  IconData? iconData;
+  String? menuTitle;
+
+  FashinexMenu({this.iconData, this.menuTitle});
+
+
+}
+///
+///  BottomNavigationBarItem(
+//               icon: Icon(Icons.home_filled),
+//               label: "Home",
+//               tooltip: "Home",
+//             ),
+//             BottomNavigationBarItem(
+//               icon: Icon(Icons.shopping_bag_outlined),
+//               label: "Cart",
+//               tooltip: "Cart",
+//             ),
+//             BottomNavigationBarItem(
+//               icon: Icon(Icons.shopping_cart_outlined),
+//               label: "Orders",
+//               tooltip: "Orders",
+//             ),
+//             BottomNavigationBarItem(
+//               icon: Icon(Icons.wallet),
+//               label: "Wallet",
+//               tooltip: "Wallet",
+//             ),
+//             BottomNavigationBarItem(
+//               icon: Icon(Icons.person_outline),
+//               label: "Profile",
+//               tooltip: "Profile",
+//             )
+List<FashinexMenu> fashinexMenuItems = [
+  FashinexMenu(
+    iconData:
+  )
+];
