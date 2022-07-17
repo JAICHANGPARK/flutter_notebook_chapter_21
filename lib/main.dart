4c18e9fddec31640ba8e96d595e6d0929f2b4246
Dreamwalker <aristojeff@gmail.com>
2022-07-17 09:32:37 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_chapter_21/ep1209_coffee_delivery_app/coffee_delivery_app.dart';
+
+import 'package:flutter_notebook_chapter_21/ep1215_fashinex_app/fashinex_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(
     ProviderScope(
-      child: CoffeeDeliveryApp(),
+      child: FashinexApp(),
     ),
   );
 }
