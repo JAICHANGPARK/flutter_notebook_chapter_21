57694ef9a7bfb6dee953d9e60110156ab292e74c
Dreamwalker <aristojeff@gmail.com>
2022-07-17 09:32:07 +0900

    Create fashinex_app.dart

---
+++ lib/ep1215_fashinex_app/fashinex_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class FashinexApp extends StatelessWidget {
+  const FashinexApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
