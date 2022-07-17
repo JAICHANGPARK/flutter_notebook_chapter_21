c35f034ff91ed2ab5ef6db8a6a18b6ef2e0084d2
Dreamwalker <aristojeff@gmail.com>
2022-07-17 09:46:46 +0900

    Update fashinex_main_page.dart

--- lib/ep1215_fashinex_app/src/ui/fashinex_main_page.dart
+++ lib/ep1215_fashinex_app/src/ui/fashinex_main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_21/ep1215_fashinex_app/src/model/fashinex_menu.dart';
 
 class FashinexMainPage extends StatefulWidget {
   const FashinexMainPage({Key? key}) : super(key: key);
@@ -20,55 +21,35 @@ class _FashinexMainPageState extends State<FashinexMainPage> {
           children: [
             SingleChildScrollView(
               child: Column(
-                children: [
-
-                ],
+                children: [],
               ),
             ),
+            Center(
+              child: Text(""),
+            )
           ],
         ),
       ),
       bottomNavigationBar: SizedBox(
         height: 72,
         child: BottomNavigationBar(
-          currentIndex: screenIndex,
-          onTap: (idx) {
-            setState(() {
-              screenIndex = idx;
-            });
-          },
-          type: BottomNavigationBarType.fixed,
-          selectedItemColor: Colors.black,
-          unselectedItemColor: Colors.grey,
-          backgroundColor: Colors.white,
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home_filled),
-              label: "Home",
-              tooltip: "Home",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_bag_outlined),
-              label: "Cart",
-              tooltip: "Cart",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_cart_outlined),
-              label: "Orders",
-              tooltip: "Orders",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.wallet),
-              label: "Wallet",
-              tooltip: "Wallet",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person_outline),
-              label: "Profile",
-              tooltip: "Profile",
-            )
-          ],
-        ),
+            currentIndex: screenIndex,
+            onTap: (idx) {
+              setState(() {
+                screenIndex = idx;
+              });
+            },
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: Colors.black,
+            unselectedItemColor: Colors.grey,
+            backgroundColor: Colors.white,
+            items: fashinexMenuItems
+                .map((e) => BottomNavigationBarItem(
+                      icon: Icon(e.iconData ?? Icons.home_max),
+                      label: e.menuTitle ?? "",
+                      tooltip: "Home",
+                    ))
+                .toList()),
       ),
     );
   }
