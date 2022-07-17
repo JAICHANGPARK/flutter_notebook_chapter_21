5c855971acacae29bd529584261a6b64e177dd1f
Dreamwalker <aristojeff@gmail.com>
2022-07-17 09:34:43 +0900

    Update fashinex_app.dart

--- lib/ep1215_fashinex_app/fashinex_app.dart
+++ lib/ep1215_fashinex_app/fashinex_app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_notebook_chapter_21/ep1215_fashinex_app/src/ui/fashinex_main_page.dart';
 
 class FashinexApp extends StatelessWidget {
   const FashinexApp({Key? key}) : super(key: key);
@@ -7,7 +7,9 @@ class FashinexApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      routes: {
+        "/": (context) => FashinexMainPage(),
+      },
     );
   }
 }
