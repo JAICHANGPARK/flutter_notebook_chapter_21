03e444e05c6a42031a36cdbc40c54a35c7c9d71a
Dreamwalker <aristojeff@gmail.com>
2022-07-17 09:34:02 +0900

    Create fashinex_main_page.dart

---
+++ lib/ep1215_fashinex_app/src/ui/fashinex_main_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class FashinexMainPage extends StatefulWidget {
+  const FashinexMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<FashinexMainPage> createState() => _FashinexMainPageState();
+}
+
+class _FashinexMainPageState extends State<FashinexMainPage> {
+  int screenIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
