add21a900bac619bda2f85f2ec90379d4975ae08
Dreamwalker <aristojeff@gmail.com>
2022-07-18 21:35:46 +0900

    Create fashinex_main_widget.dart

---
+++ lib/ep1215_fashinex_app/src/ui/widget/fashinex_main_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FashinexMainWidget extends StatefulWidget {
+  const FashinexMainWidget({Key? key}) : super(key: key);
+
+  @override
+  State<FashinexMainWidget> createState() => _FashinexMainWidgetState();
+}
+
+class _FashinexMainWidgetState extends State<FashinexMainWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
