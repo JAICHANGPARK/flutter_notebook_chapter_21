6b4f30c0255c4e43b7dab57f85e2d4346015619d
Dreamwalker <aristojeff@gmail.com>
2022-07-19 20:04:11 +0900

    Update fashinex_wish_list_page.dart

--- lib/ep1215_fashinex_app/src/ui/fashinex_wish_list_page.dart
+++ lib/ep1215_fashinex_app/src/ui/fashinex_wish_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_21/ep1215_fashinex_app/src/model/fashinex_menu.dart';
 
 class FashinexWishListPage extends StatefulWidget {
   const FashinexWishListPage({Key? key}) : super(key: key);
@@ -8,9 +9,34 @@ class FashinexWishListPage extends StatefulWidget {
 }
 
 class _FashinexWishListPageState extends State<FashinexWishListPage> {
+  int screenIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: SizedBox(
+        height: 72,
+        child: BottomNavigationBar(
+            currentIndex: screenIndex,
+            onTap: (idx) {
+              setState(() {
+                screenIndex = idx;
+              });
+            },
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: Colors.black,
+            unselectedItemColor: Colors.grey,
+            backgroundColor: Colors.white,
+            items: fashinexMenuItems
+                .map((e) => BottomNavigationBarItem(
+              icon: Icon(e.iconData ?? Icons.home_max),
+              label: e.menuTitle ?? "",
+              tooltip: "Home",
+            ))
+                .toList()),
+      ),
+
+    );
   }
 }
 
