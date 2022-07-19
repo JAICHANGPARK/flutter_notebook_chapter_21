0eea49a47aa2456744f230179c2822ee01fbba32
Dreamwalker <aristojeff@gmail.com>
2022-07-19 20:01:21 +0900

    Create fashinex_wish_list_page.dart

---
+++ lib/ep1215_fashinex_app/src/ui/fashinex_wish_list_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FashinexWishListPage extends StatefulWidget {
+  const FashinexWishListPage({Key? key}) : super(key: key);
+
+  @override
+  State<FashinexWishListPage> createState() => _FashinexWishListPageState();
+}
+
+class _FashinexWishListPageState extends State<FashinexWishListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
