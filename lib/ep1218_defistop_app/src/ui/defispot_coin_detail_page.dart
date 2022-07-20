64135d26ca38e54ddbb981d95b9759372752151d
Dreamwalker <aristojeff@gmail.com>
2022-07-20 19:55:41 +0900

    Update defispot_coin_detail_page.dart

--- lib/ep1218_defistop_app/src/ui/defispot_coin_detail_page.dart
+++ lib/ep1218_defistop_app/src/ui/defispot_coin_detail_page.dart
@@ -20,12 +20,18 @@ class _DefispotCoinDetailpageState extends State<DefispotCoinDetailpage> {
             Divider(
               color: Colors.grey,
             ),
-            Expanded(child: Placeholder()),
+            Expanded(
+              child: Placeholder(),
+            ),
             Divider(
               color: Colors.grey,
             ),
             Container(
-              height: 72,
+              height: 64,
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              margin: EdgeInsets.only(
+                bottom: 16,
+              ),
               child: Row(
                 children: [
                   OutlinedButton.icon(
@@ -36,6 +42,32 @@ class _DefispotCoinDetailpageState extends State<DefispotCoinDetailpage> {
                       primary: Colors.black,
                       shape: StadiumBorder(),
                     ),
+                  ),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Trade"),
+                    style: ElevatedButton.styleFrom(
+                      shape: StadiumBorder(),
+                      backgroundColor: Colors.black,
+                    ),
+                  ),
+                  Spacer(),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.star_border,
+                    ),
+                    color: Colors.grey,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.share_outlined,
+                    ),
+                    color: Colors.grey,
                   ),
                 ],
               ),
