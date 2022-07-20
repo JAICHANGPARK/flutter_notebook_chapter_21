c006eab19668a9b46547041d6cd9a5cd92e56b5a
Dreamwalker <aristojeff@gmail.com>
2022-07-20 19:46:18 +0900

    Create defispot_coin_detail_page.dart

---
+++ lib/ep1218_defistop_app/src/ui/defispot_coin_detail_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DefispotCoinDetailpage extends StatefulWidget {
+  const DefispotCoinDetailpage({Key? key}) : super(key: key);
+
+  @override
+  State<DefispotCoinDetailpage> createState() => _DefispotCoinDetailpageState();
+}
+
+class _DefispotCoinDetailpageState extends State<DefispotCoinDetailpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
