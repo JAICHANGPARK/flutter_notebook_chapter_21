c97c9b15816325535f1ecd290cbbbfaa216f05c9
Dreamwalker <aristojeff@gmail.com>
2022-07-20 19:47:39 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter_notebook_chapter_21/ep1215_fashinex_app/fashinex_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_notebook_chapter_21/ep1218_defistop_app/defispot_app.dart';
+
 
 void main() {
   runApp(
     ProviderScope(
-      child: FashinexApp(),
+      child: DefispotApp(),
     ),
   );
 }
