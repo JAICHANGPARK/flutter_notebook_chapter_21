f4fa6e9e8561424c899705d5239fd9cb01bec9c9
Dreamwalker <aristojeff@gmail.com>
2022-07-20 19:47:41 +0900

    Update defispot_app.dart

--- lib/ep1218_defistop_app/defispot_app.dart
+++ lib/ep1218_defistop_app/defispot_app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_notebook_chapter_21/ep1218_defistop_app/src/ui/defispot_coin_detail_page.dart';
 
 class DefispotApp extends StatelessWidget {
   const DefispotApp({Key? key}) : super(key: key);
@@ -7,6 +7,7 @@ class DefispotApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: DefispotCoinDetailpage(),
 
     );
   }
