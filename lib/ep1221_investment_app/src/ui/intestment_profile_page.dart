98b34681ce11d6c98ae39ccda9b44f9449f4b918
Dreamwalker <aristojeff@gmail.com>
2022-07-23 18:05:58 +0900

    Create intestment_profile_page.dart

---
+++ lib/ep1221_investment_app/src/ui/intestment_profile_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InvestmentProfilePage extends StatefulWidget {
+  const InvestmentProfilePage({Key? key}) : super(key: key);
+
+  @override
+  State<InvestmentProfilePage> createState() => _InvestmentProfilePageState();
+}
+
+class _InvestmentProfilePageState extends State<InvestmentProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
