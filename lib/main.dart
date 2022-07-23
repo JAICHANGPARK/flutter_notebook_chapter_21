3a284e759de537c316f0c82cde4d82914d26040f
Dreamwalker <aristojeff@gmail.com>
2022-07-23 18:05:11 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_notebook_chapter_21/ep1218_defistop_app/defispot_app.dart';
-
+import 'package:flutter_notebook_chapter_21/ep1221_investment_app/investment_app.dart';
 
 void main() {
   runApp(
     ProviderScope(
-      child: DefispotApp(),
+      child: InvestmentApp(),
     ),
   );
 }
