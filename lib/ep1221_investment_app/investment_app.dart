3ec53ebcf5a7d4ba34919ac86df04581b512b825
Dreamwalker <aristojeff@gmail.com>
2022-07-25 17:59:37 +0900

    Update investment_app.dart

--- lib/ep1221_investment_app/investment_app.dart
+++ lib/ep1221_investment_app/investment_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_21/ep1221_investment_app/src/ui/intestment_profile_page.dart';
+import 'package:flutter_notebook_chapter_21/ep1221_investment_app/src/ui/investment_home_page.dart';
 
 class InvestmentApp extends StatelessWidget {
   const InvestmentApp({Key? key}) : super(key: key);
@@ -7,8 +8,9 @@ class InvestmentApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: "/profile",
+      initialRoute: "/",
       routes: {
+        "/": (context) => InvestmentHomePage(),
         "/profile": (context) => InvestmentProfilePage(),
       },
     );
