1ef85b833efcd42edc4f1ba11b837c15b845b2e1
Dreamwalker <aristojeff@gmail.com>
2022-07-25 17:58:38 +0900

    Create investment_home_page.dart

---
+++ lib/ep1221_investment_app/src/ui/investment_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InvestmentHomePage extends StatefulWidget {
+  const InvestmentHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<InvestmentHomePage> createState() => _InvestmentHomePageState();
+}
+
+class _InvestmentHomePageState extends State<InvestmentHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
