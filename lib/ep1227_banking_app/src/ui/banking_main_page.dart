614a79c5d3575596b383020258675aaf28619314
Dreamwalker <aristojeff@gmail.com>
2022-07-29 19:25:46 +0900

    Create banking_main_page.dart

---
+++ lib/ep1227_banking_app/src/ui/banking_main_page.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class BankingMainPage extends StatefulWidget {
+  const BankingMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<BankingMainPage> createState() => _BankingMainPageState();
+}
+
+class _BankingMainPageState extends State<BankingMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
