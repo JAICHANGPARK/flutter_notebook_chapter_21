57dac50ca03cafda6d0c4d7459062a2e6615ed64
Dreamwalker <aristojeff@gmail.com>
2022-07-29 19:25:43 +0900

    Update banking_app.dart

--- lib/ep1227_banking_app/banking_app.dart
+++ lib/ep1227_banking_app/banking_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_21/ep1227_banking_app/src/ui/banking_main_page.dart';
 
 
 class BankingApp extends StatelessWidget {
@@ -7,7 +8,7 @@ class BankingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+        home: BankingMainPage(),
     );
   }
 }
