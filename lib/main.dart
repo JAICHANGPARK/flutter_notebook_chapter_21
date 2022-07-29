c1b1c2c1b7beb83372e6c2f9c172952666a38edf
Dreamwalker <aristojeff@gmail.com>
2022-07-29 19:24:32 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_21/ep1227_banking_app/banking_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_notebook_chapter_21/ep1221_investment_app/investment_app.dart';
 
 void main() {
   runApp(
     ProviderScope(
-      child: InvestmentApp(),
+      child: BankingApp(),
     ),
   );
 }
