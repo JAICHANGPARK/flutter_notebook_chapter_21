c6c859085d9026242f24a335ab50f053042f3070
Dreamwalker <aristojeff@gmail.com>
2022-07-29 19:31:11 +0900

    Update banking_main_page.dart

--- lib/ep1227_banking_app/src/ui/banking_main_page.dart
+++ lib/ep1227_banking_app/src/ui/banking_main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class BankingMainPage extends StatefulWidget {
   const BankingMainPage({Key? key}) : super(key: key);
@@ -12,6 +13,31 @@ class _BankingMainPageState extends State<BankingMainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              child: Consumer(
+                builder: (context, ref, _) {
+                  AsyncValue<int> indexValue = ref.watch(bankingMainIndexProvider);
+                  return indexValue.when(
+                      data: (d) {
+                        return Container();
+                      }, error: (e, s) {
+                    return Text(e.toString())
+                    ,
+                  }, loading: () => Center(child: CircularProgressIndicator(),));
+                },
+              ),
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0,
+            ),
+
+          ],
+        ),
+      ),
     );
   }
 }
