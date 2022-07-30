35497ff5fb386fa8ab4f50e54f55c7e9b7cddf0c
Dreamwalker <aristojeff@gmail.com>
2022-07-30 14:31:42 +0900

    Update banking_index_provider.dart

--- lib/ep1227_banking_app/src/provider/banking_index_provider.dart
+++ lib/ep1227_banking_app/src/provider/banking_index_provider.dart
@@ -4,5 +4,5 @@ final bankingMenuIndex = StateProvider<int>((ref) => 0);
 
 final bankingMainIndexProvider = FutureProvider<int>((ref) async {
   final idx = ref.watch(bankingMenuIndex);
-  return Future.delayed(Duration(seconds: 2));
+  return Future.delayed(Duration(seconds: 2)).then((value) => idx);
 });
