90617bdc6f8d1ed7ba347b2b429f2cede59d98fc
Dreamwalker <aristojeff@gmail.com>
2022-07-30 14:29:21 +0900

    Update banking_index_provider.dart

--- lib/ep1227_banking_app/src/provider/banking_index_provider.dart
+++ lib/ep1227_banking_app/src/provider/banking_index_provider.dart
@@ -2,6 +2,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final bankingMenuIndex = StateProvider<int>((ref) => 0);
 
-final bankingMainIndexProvider = FutureProvider<int>((ref) {
-  return ref.watch(bankingMenuIndex);
+final bankingMainIndexProvider = FutureProvider<int>((ref) async {
+  final idx = ref.watch(bankingMenuIndex);
+  await Future.delayed(Duration(seconds: 2));
+  return idx;
 });
