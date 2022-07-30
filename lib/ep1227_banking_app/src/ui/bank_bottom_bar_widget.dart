223a21bd9202417f291ac63a37b60f1618b0b548
Dreamwalker <aristojeff@gmail.com>
2022-07-30 14:47:39 +0900

    Create bank_bottom_bar_widget.dart

---
+++ lib/ep1227_banking_app/src/ui/bank_bottom_bar_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+
+class BankBottomBarWidget extends ConsumerWidget {
+  const BankBottomBarWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
