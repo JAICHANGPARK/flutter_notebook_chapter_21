a2c602def6da6160ee69568894d8e0f6bdaea2d2
Dreamwalker <aristojeff@gmail.com>
2022-07-30 14:48:24 +0900

    Update banking_main_page.dart

--- lib/ep1227_banking_app/src/ui/banking_main_page.dart
+++ lib/ep1227_banking_app/src/ui/banking_main_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_21/ep1227_banking_app/src/provider/banking_index_provider.dart';
+import 'package:flutter_notebook_chapter_21/ep1227_banking_app/src/ui/bank_bottom_bar_widget.dart';
 import 'package:flutter_notebook_chapter_21/ep1227_banking_app/src/ui/bank_home_widget.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -59,56 +60,7 @@ class _BankingMainPageState extends State<BankingMainPage> {
               bottom: 0,
               right: 0,
               left: 0,
-              child: Consumer(builder: (context, ref, _) {
-                final index = ref.watch(bankingMenuIndex);
-                return Container(
-                  height: 84,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(8),
-                      topLeft: Radius.circular(8),
-                    ),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      IconButton(
-                        iconSize: 32,
-                        onPressed: () {
-                          ref.read(bankingMenuIndex.notifier).state = 0;
-                        },
-                        icon: Icon(Icons.home_filled),
-                        color: index == 0 ? Colors.blueAccent : Colors.grey,
-                      ),
-                      IconButton(
-                        iconSize: 32,
-                        onPressed: () {
-                          ref.read(bankingMenuIndex.notifier).state = 1;
-                        },
-                        icon: Icon(Icons.bar_chart),
-                        color: index == 1 ? Colors.blueAccent : Colors.grey,
-                      ),
-                      IconButton(
-                        iconSize: 32,
-                        onPressed: () {
-                          ref.read(bankingMenuIndex.notifier).state = 2;
-                        },
-                        icon: Icon(Icons.add_card_outlined),
-                        color: index ==2 ? Colors.blueAccent : Colors.grey,
-                      ),
-                      IconButton(
-                        iconSize: 32,
-                        onPressed: () {
-                          ref.read(bankingMenuIndex.notifier).state = 3;
-                        },
-                        icon: Icon(Icons.account_circle_outlined),
-                        color: index == 3 ? Colors.blueAccent : Colors.grey,
-                      )
-                    ],
-                  ),
-                );
-              }),
+              child: BankBottomBarWidget(),
             )
           ],
         ),
