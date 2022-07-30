1f9f2428ae9777be13bdeeb0be1c350ed6038c8d
Dreamwalker <aristojeff@gmail.com>
2022-07-30 14:46:19 +0900

    Update banking_main_page.dart

--- lib/ep1227_banking_app/src/ui/banking_main_page.dart
+++ lib/ep1227_banking_app/src/ui/banking_main_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_21/ep1227_banking_app/src/provider/banking_index_provider.dart';
+import 'package:flutter_notebook_chapter_21/ep1227_banking_app/src/ui/bank_home_widget.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class BankingMainPage extends StatefulWidget {
@@ -30,134 +31,7 @@ class _BankingMainPageState extends State<BankingMainPage> {
                     data: (d) {
                       switch (d) {
                         case 0:
-                          return SingleChildScrollView(
-                              child: Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 16),
-                            child: Column(
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.only(top: 16),
-                                  child: Row(
-                                    children: [
-                                      CircleAvatar(
-                                        backgroundColor: Colors.grey,
-                                      ),
-                                      SizedBox(
-                                        width: 16,
-                                      ),
-                                      Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            "Dreamwalker",
-                                            style: TextStyle(
-                                              color: Colors.white,
-                                            ),
-                                          ),
-                                          SizedBox(
-                                            height: 6,
-                                          ),
-                                          Row(
-                                            children: [
-                                              Text(
-                                                "Switch account",
-                                                style: TextStyle(
-                                                  color: Colors.grey,
-                                                ),
-                                              ),
-                                              Icon(
-                                                Icons.arrow_drop_down,
-                                                color: Colors.grey,
-                                              ),
-                                            ],
-                                          )
-                                        ],
-                                      ),
-                                      Spacer(),
-                                      IconButton(
-                                        onPressed: () {},
-                                        icon: Icon(
-                                          CupertinoIcons.search,
-                                        ),
-                                        color: Colors.white,
-                                      )
-                                    ],
-                                  ),
-                                ),
-                                Container(
-                                  margin: EdgeInsets.symmetric(vertical: 16),
-                                  decoration: BoxDecoration(
-                                    color: Colors.blueAccent,
-                                    borderRadius: BorderRadius.circular(4),
-                                  ),
-                                  padding: EdgeInsets.symmetric(
-                                    horizontal: 16,
-                                    vertical: 8,
-                                  ),
-                                  child: Row(
-                                    children: [
-                                      Icon(
-                                        Icons.notifications,
-                                        color: Colors.white,
-                                        size: 20,
-                                      ),
-                                      SizedBox(
-                                        width: 6,
-                                      ),
-                                      Text(
-                                        "12 New notification",
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 36,
-                                  child: ListView(
-                                    shrinkWrap: true,
-                                    scrollDirection: Axis.horizontal,
-                                    children: [
-                                      Container(
-                                        margin: EdgeInsets.only(right: 16),
-                                        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-                                        decoration: BoxDecoration(
-                                          color: Colors.white.withOpacity(0.2),
-                                          borderRadius: BorderRadius.circular(4),
-                                        ),
-                                        child: Center(
-                                          child: Text(
-                                            "Accounts",
-                                            style: TextStyle(
-                                              color: Colors.white,
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                      Container(
-                                        margin: EdgeInsets.only(right: 16),
-                                        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-                                        decoration: BoxDecoration(
-                                          color: Colors.white.withOpacity(0),
-                                          borderRadius: BorderRadius.circular(4),
-                                        ),
-                                        child: Center(
-                                          child: Text(
-                                            "Cards",
-                                            style: TextStyle(
-                                              color: Colors.grey,
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-
-                              ],
-                            ),
-                          ));
+                          return BankHomeWidget();
                       }
                       return Container(
                         child: Center(
