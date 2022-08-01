9faed17d646b63cfa8be5011b62c443360cfb26f
Dreamwalker <aristojeff@gmail.com>
2022-08-01 21:44:13 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_21/ep1227_banking_app/banking_app.dart';
+import 'package:flutter_notebook_chapter_21/ep1230_classes_app/classes_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(
     ProviderScope(
-      child: BankingApp(),
+      child: ClassesMobileApp(),
     ),
   );
 }
