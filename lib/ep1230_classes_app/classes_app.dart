b04820e98690ca9ad0eb7242d8f110b2ca58c977
Dreamwalker <aristojeff@gmail.com>
2022-08-01 21:43:35 +0900

    Create classes_app.dart

---
+++ lib/ep1230_classes_app/classes_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class ClassesMobileApp extends StatelessWidget {
+  const ClassesMobileApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
