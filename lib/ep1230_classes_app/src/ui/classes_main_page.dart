8297124186418051c002409116b5d7b7d4e96775
Dreamwalker <aristojeff@gmail.com>
2022-08-01 21:46:51 +0900

    Create classes_main_page.dart

---
+++ lib/ep1230_classes_app/src/ui/classes_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ClassesMainPage extends StatefulWidget {
+  const ClassesMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<ClassesMainPage> createState() => _ClassesMainPageState();
+}
+
+class _ClassesMainPageState extends State<ClassesMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
