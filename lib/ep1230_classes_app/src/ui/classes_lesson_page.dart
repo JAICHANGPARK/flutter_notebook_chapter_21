fc2f675ebd786c80fc3338c8a7f7908546be5eda
Dreamwalker <aristojeff@gmail.com>
2022-08-03 17:53:16 +0900

    Create classes_lesson_page.dart

---
+++ lib/ep1230_classes_app/src/ui/classes_lesson_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ClassedLessonPage extends StatefulWidget {
+  const ClassedLessonPage({Key? key}) : super(key: key);
+
+  @override
+  State<ClassedLessonPage> createState() => _ClassedLessonPageState();
+}
+
+class _ClassedLessonPageState extends State<ClassedLessonPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
