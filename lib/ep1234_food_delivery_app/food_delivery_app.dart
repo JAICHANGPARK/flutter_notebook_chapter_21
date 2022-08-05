627f66b2eb34dd074620b8fc45dc1cc74819f9e2
Dreamwalker <aristojeff@gmail.com>
2022-08-05 18:58:55 +0900

    Update food_delivery_app.dart

--- lib/ep1234_food_delivery_app/food_delivery_app.dart
+++ lib/ep1234_food_delivery_app/food_delivery_app.dart
@@ -20,14 +20,14 @@ class FoodDeliveryApp extends StatelessWidget {
             unselectedItemColor: Colors.grey,
             currentIndex: index,
             onTap: (idx) {
-              
+              ref.refresh(fdPageIndex.state).state = idx;
             },
             items: [
               BottomNavigationBarItem(icon: Icon(Icons.add_home_work_outlined), label: "Home"),
-              BottomNavigationBarItem(icon: Icon(Icons.add_home_work_outlined), label: "Orders"),
-              BottomNavigationBarItem(icon: Icon(Icons.add_home_work_outlined), label: "7Kmart"),
-              BottomNavigationBarItem(icon: Icon(Icons.add_home_work_outlined), label: "Pro"),
-              BottomNavigationBarItem(icon: Icon(Icons.add_home_work_outlined), label: "Account"),
+              BottomNavigationBarItem(icon: Icon(Icons.list_alt), label: "Orders"),
+              BottomNavigationBarItem(icon: Icon(Icons.inventory_2_outlined), label: "7Kmart"),
+              BottomNavigationBarItem(icon: Icon(Icons.stars_outlined), label: "Pro"),
+              BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: "Account"),
             ],
           );
         }),
