9e2a1ca17f5376e707d516469bd83eec66ec9671
Dreamwalker <aristojeff@gmail.com>
2022-08-06 17:20:39 +0900

    Update food_delivery_app.dart

--- lib/ep1234_food_delivery_app/food_delivery_app.dart
+++ lib/ep1234_food_delivery_app/food_delivery_app.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_chapter_21/ep1234_food_delivery_app/src/provider/fd_page_index.dart';
 import 'package:flutter_notebook_chapter_21/ep1234_food_delivery_app/src/ui/food_delivery_home_page.dart';
-import 'package:flutter_notebook_chapter_21/ep1234_food_delivery_app/src/ui/widget/fd_bottom_bar_widget.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FoodDeliveryApp extends StatelessWidget {
