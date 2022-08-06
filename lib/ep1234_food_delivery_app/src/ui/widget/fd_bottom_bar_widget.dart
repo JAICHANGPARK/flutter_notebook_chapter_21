eb54ad35fedfcb9dabb91db3e98c9af083b751d6
Dreamwalker <aristojeff@gmail.com>
2022-08-06 17:12:20 +0900

    Update fd_bottom_bar_widget.dart

---
+++ lib/ep1234_food_delivery_app/src/ui/widget/fd_bottom_bar_widget.dart
@@ -0,0 +1,10 @@
+
+
+class FdBottomBarWidget extends StatelessWidget {
+  const FdBottomBarWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
