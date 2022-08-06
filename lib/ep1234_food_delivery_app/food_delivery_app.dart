df68c0b5fea95279f2703945142b983deb120fef
Dreamwalker <aristojeff@gmail.com>
2022-08-06 17:14:28 +0900

    Update food_delivery_app.dart

--- lib/ep1234_food_delivery_app/food_delivery_app.dart
+++ lib/ep1234_food_delivery_app/food_delivery_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_21/ep1234_food_delivery_app/src/provider/fd_page_index.dart';
+import 'package:flutter_notebook_chapter_21/ep1234_food_delivery_app/src/ui/widget/fd_bottom_bar_widget.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FoodDeliveryApp extends StatelessWidget {
@@ -98,25 +99,7 @@ class FoodDeliveryApp extends StatelessWidget {
             ),
           );
         }),
-        bottomNavigationBar: Consumer(builder: (context, ref, _) {
-          final index = ref.watch(fdPageIndex);
-          return BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            selectedItemColor: Colors.teal,
-            unselectedItemColor: Colors.grey,
-            currentIndex: index,
-            onTap: (idx) {
-              ref.refresh(fdPageIndex.state).state = idx;
-            },
-            items: [
-              BottomNavigationBarItem(icon: Icon(Icons.add_home_work_outlined), label: "Home"),
-              BottomNavigationBarItem(icon: Icon(Icons.list_alt), label: "Orders"),
-              BottomNavigationBarItem(icon: Icon(Icons.inventory_2_outlined), label: "7Kmart"),
-              BottomNavigationBarItem(icon: Icon(Icons.stars_outlined), label: "Pro"),
-              BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: "Account"),
-            ],
-          );
-        }),
+        bottomNavigationBar: FdBottomBarWidget(),
       ),
     );
   }
