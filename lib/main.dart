666b4f2a5f3100882b130be9d27e911dd483129a
Dreamwalker <aristojeff@gmail.com>
2022-08-08 17:34:44 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_chapter_21/ep1227_banking_app/banking_app.dart';
-import 'package:flutter_notebook_chapter_21/ep1230_classes_app/classes_app.dart';
-import 'package:flutter_notebook_chapter_21/ep1234_food_delivery_app/food_delivery_app.dart';
+import 'package:flutter_notebook_chapter_21/ep1237_job_finder_app/job_finder_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(
     ProviderScope(
-      child: FoodDeliveryApp(),
+      child: JobFinderApp(),
     ),
   );
 }
