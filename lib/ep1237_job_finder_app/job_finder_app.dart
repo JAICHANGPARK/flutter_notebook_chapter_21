136b7a0d5ff7ba575a99d381f89a99ccb877fbaf
Dreamwalker <aristojeff@gmail.com>
2022-08-08 17:33:49 +0900

    Update job_finder_app.dart

--- lib/ep1237_job_finder_app/job_finder_app.dart
+++ lib/ep1237_job_finder_app/job_finder_app.dart
@@ -1,6 +1,5 @@
-
 import 'package:flutter/material.dart';
-
+import 'package:flutter_notebook_chapter_21/ep1237_job_finder_app/src/ui/job_finder_start_page.dart';
 
 class JobFinderApp extends StatelessWidget {
   const JobFinderApp({Key? key}) : super(key: key);
@@ -8,7 +7,7 @@ class JobFinderApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ,
+      home: JobFinderStartPage()s,
     );
   }
 }
