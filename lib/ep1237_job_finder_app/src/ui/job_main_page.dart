503b8d72a1ff1ac8bff3353515e436262b9f6d78
Dreamwalker <aristojeff@gmail.com>
2022-08-08 17:51:14 +0900

    Create job_main_page.dart

---
+++ lib/ep1237_job_finder_app/src/ui/job_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class JobMainPage extends StatefulWidget {
+  const JobMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<JobMainPage> createState() => _JobMainPageState();
+}
+
+class _JobMainPageState extends State<JobMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
