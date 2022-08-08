4dbbd769a01bb884e2d26eb4be92d0418c14245e
Dreamwalker <aristojeff@gmail.com>
2022-08-08 17:33:01 +0900

    Create job_finder_app.dart

---
+++ lib/ep1237_job_finder_app/job_finder_app.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+
+class JobFinderApp extends StatelessWidget {
+  const JobFinderApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ,
+    );
+  }
+}
