e077ba27d0092272d1bc2d2d9170123d543687e8
Dreamwalker <aristojeff@gmail.com>
2022-08-08 17:33:47 +0900

    Update job_finder_start_page.dart

--- lib/ep1237_job_finder_app/src/ui/job_finder_start_page.dart
+++ lib/ep1237_job_finder_app/src/ui/job_finder_start_page.dart
@@ -1,1 +1,11 @@
 
+import 'package:flutter/material.dart';
+
+class JobFinderStartPage extends StatelessWidget {
+  const JobFinderStartPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
