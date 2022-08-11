fd81c20f6266357b4e5e4c13f08a8016eed1dd0c
Dreamwalker <aristojeff@gmail.com>
2022-08-11 19:12:15 +0900

    Create job_description_page.dart

---
+++ lib/ep1237_job_finder_app/src/ui/job_description_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class JobDescriptionPage extends StatefulWidget {
+  const JobDescriptionPage({Key? key}) : super(key: key);
+
+  @override
+  State<JobDescriptionPage> createState() => _JobDescriptionPageState();
+}
+
+class _JobDescriptionPageState extends State<JobDescriptionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
