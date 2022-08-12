fae23c9dc87e5208fe89c45fb8620e78233c4fa7
Dreamwalker <aristojeff@gmail.com>
2022-08-12 16:41:02 +0900

    Create skyrent_app.dart

---
+++ lib/ep1241_skyrent_app/skyrent_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class SkylentApp extends StatelessWidget {
+  const SkylentApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SkyrentStartPage(),
+    );
+  }
+}
