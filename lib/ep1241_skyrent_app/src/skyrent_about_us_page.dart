a7ef19d3be964c8b5c07d4eeb5535665f446537d
Dreamwalker <aristojeff@gmail.com>
2022-08-12 16:55:14 +0900

    Create skyrent_about_us_page.dart

---
+++ lib/ep1241_skyrent_app/src/skyrent_about_us_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class SkyrentAboutUsPage extends StatelessWidget {
+  const SkyrentAboutUsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
