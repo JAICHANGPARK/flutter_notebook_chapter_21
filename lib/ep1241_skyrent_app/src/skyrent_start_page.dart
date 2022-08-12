ab4118f0c9470845b474a9f94b0e433374396aab
Dreamwalker <aristojeff@gmail.com>
2022-08-12 16:43:00 +0900

    Update skyrent_start_page.dart

--- lib/ep1241_skyrent_app/src/skyrent_start_page.dart
+++ lib/ep1241_skyrent_app/src/skyrent_start_page.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 
 class SkyrentStartPage extends StatelessWidget {
   const SkyrentStartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(
+                  "Skyrent",
+                  style: GoogleFonts.federo(
+                    fontSize: 28,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
