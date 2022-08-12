dfcc581ee14a2a49b6031051d43f13199e682b4f
Dreamwalker <aristojeff@gmail.com>
2022-08-12 16:46:15 +0900

    Update skyrent_start_page.dart

--- lib/ep1241_skyrent_app/src/skyrent_start_page.dart
+++ lib/ep1241_skyrent_app/src/skyrent_start_page.dart
@@ -9,9 +9,10 @@ class SkyrentStartPage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(12.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -31,14 +32,27 @@ class SkyrentStartPage extends StatelessWidget {
                 ],
               ),
             ),
-            Text(
-              "APARTMENTS",
-              style: GoogleFonts.federo(
-                fontSize: 32,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            Text("FOR RENT IN NYC")
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "APARTMENTS",
+                  style: GoogleFonts.federo(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Text(
+                  "FOR RENT IN NYC",
+                  style: GoogleFonts.federo(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Text("Search for luxury apartments"),
+                Text('in NYC according to your criteria')
+              ],
+            )
           ],
         ),
       ),
