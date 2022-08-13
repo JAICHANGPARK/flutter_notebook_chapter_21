abbe41f64719be3943d586a1916b0f41596954be
Dreamwalker <aristojeff@gmail.com>
2022-08-13 14:55:03 +0900

    Update skyrent_about_us_page.dart

--- lib/ep1241_skyrent_app/src/skyrent_about_us_page.dart
+++ lib/ep1241_skyrent_app/src/skyrent_about_us_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+TextStyle skyrentTitle = GoogleFonts.federo(
+  fontSize: 42,
+  fontWeight: FontWeight.w600,
+);
 
 class SkyrentAboutUsPage extends StatelessWidget {
   const SkyrentAboutUsPage({Key? key}) : super(key: key);
@@ -7,7 +12,27 @@ class SkyrentAboutUsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "ABOUT US",
+                style: GoogleFonts.federo(
+                  fontSize: 32,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Text("Our service since 1999 always allows"),
+              Text("you to quickly find a comforatable apartment"),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
