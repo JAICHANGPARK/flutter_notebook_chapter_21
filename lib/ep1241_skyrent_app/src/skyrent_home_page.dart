e6931567f739a50774a781de85057874965374b5
Dreamwalker <aristojeff@gmail.com>
2022-08-14 16:51:55 +0900

    Create skyrent_home_page.dart

---
+++ lib/ep1241_skyrent_app/src/skyrent_home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class SkylentHomePage extends StatelessWidget {
+  const SkylentHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
