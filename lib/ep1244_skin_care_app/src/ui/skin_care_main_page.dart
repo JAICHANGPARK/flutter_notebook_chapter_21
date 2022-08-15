23dfa85324d59675d7fd41afea12d48d025de9bb
Dreamwalker <aristojeff@gmail.com>
2022-08-15 14:02:30 +0900

    Create skin_care_main_page.dart

---
+++ lib/ep1244_skin_care_app/src/ui/skin_care_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SkinCareMainPage extends StatefulWidget {
+  const SkinCareMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<SkinCareMainPage> createState() => _SkinCareMainPageState();
+}
+
+class _SkinCareMainPageState extends State<SkinCareMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
