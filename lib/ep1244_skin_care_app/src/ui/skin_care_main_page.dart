6d17e17fa03dcd5fd85bb0edcdf6ed8625cc7bb8
Dreamwalker <aristojeff@gmail.com>
2022-08-15 14:13:39 +0900

    Update skin_care_main_page.dart

--- lib/ep1244_skin_care_app/src/ui/skin_care_main_page.dart
+++ lib/ep1244_skin_care_app/src/ui/skin_care_main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SkinCareMainPage extends StatefulWidget {
   const SkinCareMainPage({Key? key}) : super(key: key);
@@ -15,6 +16,9 @@ class _SkinCareMainPageState extends State<SkinCareMainPage> {
       body: SafeArea(
         child: Column(
           children: [
+            SizedBox(
+              height: 8,
+            ),
             Row(
               children: [
                 IconButton(
@@ -24,9 +28,16 @@ class _SkinCareMainPageState extends State<SkinCareMainPage> {
                   ),
                 ),
                 Expanded(
-                    child: Center(
-                  child: Text("SkinLab"),
-                )),
+                  child: Center(
+                    child: Text(
+                      "SkinLab",
+                      style: GoogleFonts.federo(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ),
+                ),
                 Container(
                   height: 38,
                   width: 38,
@@ -46,10 +57,17 @@ class _SkinCareMainPageState extends State<SkinCareMainPage> {
                       ),
                       Positioned(
                         left: 6,
-                        bottom: 4,
+                        bottom: 2,
                         child: CircleAvatar(
                           radius: 6,
                           backgroundColor: Colors.black,
+                          foregroundColor: Colors.white,
+                          child: Text(
+                            "3",
+                            style: TextStyle(
+                              fontSize: 10,
+                            ),
+                          ),
                         ),
                       )
                     ],
