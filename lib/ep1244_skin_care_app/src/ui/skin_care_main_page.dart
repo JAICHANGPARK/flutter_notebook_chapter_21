c4125ac8b4de9a43cd84eeca0c7fbac22790b8e3
Dreamwalker <aristojeff@gmail.com>
2022-08-15 14:07:52 +0900

    Update skin_care_main_page.dart

--- lib/ep1244_skin_care_app/src/ui/skin_care_main_page.dart
+++ lib/ep1244_skin_care_app/src/ui/skin_care_main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class SkinCareMainPage extends StatefulWidget {
   const SkinCareMainPage({Key? key}) : super(key: key);
 
@@ -14,7 +13,52 @@ class _SkinCareMainPageState extends State<SkinCareMainPage> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-
+        child: Column(
+          children: [
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.menu,
+                  ),
+                ),
+                Expanded(
+                    child: Center(
+                  child: Text("SkinLab"),
+                )),
+                Container(
+                  height: 38,
+                  width: 38,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                        top: 0,
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.shopping_bag_outlined,
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 6,
+                        bottom: 4,
+                        child: CircleAvatar(
+                          radius: 6,
+                          backgroundColor: Colors.black,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
