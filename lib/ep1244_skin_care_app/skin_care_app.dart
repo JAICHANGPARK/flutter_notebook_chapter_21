836c980e97bf1ae92ab61ccda1b1de2ce8c59dfc
Dreamwalker <aristojeff@gmail.com>
2022-08-15 14:03:08 +0900

    Create skin_care_app.dart

---
+++ lib/ep1244_skin_care_app/skin_care_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class SkinCareApp extends StatelessWidget {
+  const SkinCareApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
+
