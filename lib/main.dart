2d623644aa3efcf49e7f02faae0abb6f3f23a46b
Dreamwalker <aristojeff@gmail.com>
2022-08-15 14:03:39 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_chapter_21/ep1237_job_finder_app/job_finder_app.dart';
-import 'package:flutter_notebook_chapter_21/ep1241_skyrent_app/skyrent_app.dart';
+import 'package:flutter_notebook_chapter_21/ep1244_skin_care_app/skin_care_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(
     ProviderScope(
-      child: SkylentApp(),
+      child: SkinCareApp(),
     ),
   );
 }
