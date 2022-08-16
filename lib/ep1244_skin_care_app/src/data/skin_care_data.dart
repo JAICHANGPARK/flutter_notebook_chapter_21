533873c98dfc35b0e62d677b1f976d0c251a6125
Dreamwalker <aristojeff@gmail.com>
2022-08-16 17:32:26 +0900

    Update skin_care_data.dart

--- lib/ep1244_skin_care_app/src/data/skin_care_data.dart
+++ lib/ep1244_skin_care_app/src/data/skin_care_data.dart
@@ -4,7 +4,12 @@ import 'package:flutter_notebook_chapter_21/ep1244_skin_care_app/src/model/skin_
 
 List<SkinCareItem> skinCareHomeItems = [
   SkinCareItem(
-    img: ""
+    img: "https://cdn.pixabay.com/photo/2017/03/30/18/17/girl-2189247_960_720.jpg",
+    title: "Facial Tools",
+  ),
+  SkinCareItem(
+    img: "https://cdn.pixabay.com/photo/2017/12/01/08/02/paint-2990357__340.jpg",
+    title: "Creams",
   )
 
 ];
