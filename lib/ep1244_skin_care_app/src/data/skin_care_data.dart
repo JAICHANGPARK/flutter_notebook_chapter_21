bed90c90fc9c8a7e1cd2725ec7f03c22c30faf7d
Dreamwalker <aristojeff@gmail.com>
2022-08-16 17:30:10 +0900

    Create skin_care_data.dart

---
+++ lib/ep1244_skin_care_app/src/data/skin_care_data.dart
@@ -0,0 +1,10 @@
+
+
+import 'package:flutter_notebook_chapter_21/ep1244_skin_care_app/src/model/skin_care_item.dart';
+
+List<SkinCareItem> skinCareHomeItems = [
+  SkinCareItem(
+    img: ""
+  )
+
+];
