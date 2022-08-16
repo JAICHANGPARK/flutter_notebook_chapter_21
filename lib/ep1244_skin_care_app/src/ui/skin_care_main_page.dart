df02f874a405cfdfb874655e3243e5517bbaecb1
Dreamwalker <aristojeff@gmail.com>
2022-08-16 17:27:59 +0900

    Update skin_care_main_page.dart

--- lib/ep1244_skin_care_app/src/ui/skin_care_main_page.dart
+++ lib/ep1244_skin_care_app/src/ui/skin_care_main_page.dart
@@ -131,9 +131,16 @@ class _SkinCareMainPageState extends State<SkinCareMainPage> {
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Expanded(
-                                    child: Container(
-                                  decoration: BoxDecoration(),
-                                )),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                        image: NetworkImage(
+                                            "https://cdn.pixabay.com/photo/2017/03/30/18/17/girl-2189247_960_720.jpg"),
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                ),
                                 Padding(
                                   padding: const EdgeInsets.all(8.0),
                                   child: Text(
