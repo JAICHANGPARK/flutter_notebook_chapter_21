8fe1cc35f4e539bec2ee363c114a0c19097f6caa
Dreamwalker <aristojeff@gmail.com>
2022-08-17 16:46:59 +0900

    Update skin_detail_page.dart

--- lib/ep1244_skin_care_app/src/ui/skin_detail_page.dart
+++ lib/ep1244_skin_care_app/src/ui/skin_detail_page.dart
@@ -13,68 +13,120 @@ class _SkinDetailPageState extends State<SkinDetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 8,
-            ),
-            Row(
-              children: [
-                IconButton(
-                  onPressed: () => Navigator.of(context).pop(),
-                  icon: Icon(
-                    Icons.arrow_back_ios_new,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 8,
+            horizontal: 16,
+          ),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 8,
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    icon: Icon(
+                      Icons.arrow_back_ios_new,
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: Center(
-                    child: Text(
-                      "SkinLab",
-                      style: GoogleFonts.cormorantGaramond(
-                        fontSize: 28,
-                        // fontWeight: FontWeight.w300,
+                  Expanded(
+                    child: Center(
+                      child: Text(
+                        "SkinLab",
+                        style: GoogleFonts.cormorantGaramond(
+                          fontSize: 28,
+                          // fontWeight: FontWeight.w300,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Container(
-                  height: 38,
-                  width: 38,
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        left: 0,
-                        right: 0,
-                        bottom: 0,
-                        top: 0,
-                        child: IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.shopping_bag_outlined,
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        left: 6,
-                        bottom: 2,
-                        child: CircleAvatar(
-                          radius: 6,
-                          backgroundColor: Colors.black,
-                          foregroundColor: Colors.white,
-                          child: Text(
-                            "3",
-                            style: TextStyle(
-                              fontSize: 10,
+                  Container(
+                    height: 38,
+                    width: 38,
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          left: 0,
+                          right: 0,
+                          bottom: 0,
+                          top: 0,
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.shopping_bag_outlined,
                             ),
                           ),
                         ),
-                      )
-                    ],
+                        Positioned(
+                          left: 6,
+                          bottom: 2,
+                          child: CircleAvatar(
+                            radius: 6,
+                            backgroundColor: Colors.black,
+                            foregroundColor: Colors.white,
+                            child: Text(
+                              "3",
+                              style: TextStyle(
+                                fontSize: 10,
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                )
-              ],
-            ),
-          ],
+                ],
+              ),
+              Expanded(child: Placeholder()),
+              Container(
+                height: 64,
+                decoration: BoxDecoration(border: Border.all()),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                      ),
+                      child: Row(
+                        children: [
+                          Icon(Icons.remove),
+                          Text("01"),
+                          Icon(Icons.add),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Color.fromRGBO(56, 63, 70, 1),
+                        ),
+                        child: Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.shopping_bag_outlined,
+                                color: Colors.white,
+                              ),
+                              SizedBox(
+                                width: 16,
+                              ),
+                              Text(
+                                "\$ 39.99",
+                                style: GoogleFonts.cormorantGaramond(fontSize: 24, color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
