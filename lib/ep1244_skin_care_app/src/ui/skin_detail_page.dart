10f1766c39b339bacc5c8ad796546e1389f8fa28
Dreamwalker <aristojeff@gmail.com>
2022-08-17 16:37:31 +0900

    Update skin_detail_page.dart

---
+++ lib/ep1244_skin_care_app/src/ui/skin_detail_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SkinDetailPage extends StatefulWidget {
+  const SkinDetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<SkinDetailPage> createState() => _SkinDetailPageState();
+}
+
+class _SkinDetailPageState extends State<SkinDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
