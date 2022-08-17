aae6ab6d10aa9a10b987cf260546ae339580cdf7
Dreamwalker <aristojeff@gmail.com>
2022-08-17 16:41:46 +0900

    Update skin_detail_page.dart

--- lib/ep1244_skin_care_app/src/ui/skin_detail_page.dart
+++ lib/ep1244_skin_care_app/src/ui/skin_detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SkinDetailPage extends StatefulWidget {
   const SkinDetailPage({Key? key}) : super(key: key);
@@ -10,6 +11,72 @@ class SkinDetailPage extends StatefulWidget {
 class _SkinDetailPageState extends State<SkinDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 8,
+            ),
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  icon: Icon(
+                    Icons.arrow_back_ios_new,
+                  ),
+                ),
+                Expanded(
+                  child: Center(
+                    child: Text(
+                      "SkinLab",
+                      style: GoogleFonts.cormorantGaramond(
+                        fontSize: 28,
+                        // fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 38,
+                  width: 38,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                        top: 0,
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.shopping_bag_outlined,
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 6,
+                        bottom: 2,
+                        child: CircleAvatar(
+                          radius: 6,
+                          backgroundColor: Colors.black,
+                          foregroundColor: Colors.white,
+                          child: Text(
+                            "3",
+                            style: TextStyle(
+                              fontSize: 10,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
