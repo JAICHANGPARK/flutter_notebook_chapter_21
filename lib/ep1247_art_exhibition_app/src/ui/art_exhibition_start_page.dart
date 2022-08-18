11bb0640b310a73069b27cdea808c6a2670268e4
Dreamwalker <aristojeff@gmail.com>
2022-08-18 19:16:00 +0900

    Update art_exhibition_start_page.dart

--- lib/ep1247_art_exhibition_app/src/ui/art_exhibition_start_page.dart
+++ lib/ep1247_art_exhibition_app/src/ui/art_exhibition_start_page.dart
@@ -21,9 +21,31 @@ class ArtExhibitonStartPage extends StatelessWidget {
             top: 72,
             bottom: 0,
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   "Arexs",
+                ),
+                Text(
+                  "Discover amazing art around the world",
+                  style: TextStyle(fontSize: 32),
+                ),
+                Text(
+                  "Feel the experience of seeing art from around the world in the palm of your"
+                  "hard without having to come directly to the museum",
+                ),
+                Container(
+                  height: 64,
+                  decoration: BoxDecoration(color: Colors.orangeAccent),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        child: Container(
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
                 )
               ],
             ),
