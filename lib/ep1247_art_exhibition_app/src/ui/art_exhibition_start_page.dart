62e7ac4a8398f54a7da12be098bfb32627bf671d
Dreamwalker <aristojeff@gmail.com>
2022-08-18 19:12:37 +0900

    Update art_exhibition_start_page.dart

--- lib/ep1247_art_exhibition_app/src/ui/art_exhibition_start_page.dart
+++ lib/ep1247_art_exhibition_app/src/ui/art_exhibition_start_page.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
 
-
 class ArtExhibitonStartPage extends StatelessWidget {
   const ArtExhibitonStartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            child: Container(),
+          ),
+          Positioned(
+            child: Column(
+              children: [
+                
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
