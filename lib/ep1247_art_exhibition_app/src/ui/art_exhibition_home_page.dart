f1f2a8292f3f8045d6bcf4a04ae44daa085f0bb3
Dreamwalker <aristojeff@gmail.com>
2022-08-18 19:23:59 +0900

    Create art_exhibition_home_page.dart

---
+++ lib/ep1247_art_exhibition_app/src/ui/art_exhibition_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ArtExhibitionHomePage extends StatefulWidget {
+  const ArtExhibitionHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<ArtExhibitionHomePage> createState() => _ArtExhibitionHomePageState();
+}
+
+class _ArtExhibitionHomePageState extends State<ArtExhibitionHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
