3df8495d79f671e538eba80f2fd9b21d36bf91ed
Dreamwalker <aristojeff@gmail.com>
2022-08-19 16:44:54 +0900

    Update art_exhibition_home_page.dart

--- lib/ep1247_art_exhibition_app/src/ui/art_exhibition_home_page.dart
+++ lib/ep1247_art_exhibition_app/src/ui/art_exhibition_home_page.dart
@@ -10,6 +10,39 @@ class ArtExhibitionHomePage extends StatefulWidget {
 class _ArtExhibitionHomePageState extends State<ArtExhibitionHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    // Colors.black.withOpacity(0.2),
+                    // Colors.black.withOpacity(0.3),
+                    // Colors.black.withOpacity(0.4),
+                    // Colors.black.withOpacity(0.5),
+                    Colors.black.withOpacity(0.8),
+                    Colors.black,
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
