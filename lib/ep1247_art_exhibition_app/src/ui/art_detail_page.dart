296b362471c879e79942356d37e02b7d99fff399
Dreamwalker <aristojeff@gmail.com>
2022-08-20 12:36:15 +0900

    Create art_detail_page.dart

---
+++ lib/ep1247_art_exhibition_app/src/ui/art_detail_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class ArtDetailPage extends StatefulWidget {
+  const ArtDetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<ArtDetailPage> createState() => _ArtDetailPageState();
+}
+
+class _ArtDetailPageState extends State<ArtDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
