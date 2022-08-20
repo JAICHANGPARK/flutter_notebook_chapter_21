1be9378497579c27124a588ae1a9ca931f125cc3
Dreamwalker <aristojeff@gmail.com>
2022-08-20 12:39:10 +0900

    Update art_detail_page.dart

--- lib/ep1247_art_exhibition_app/src/ui/art_detail_page.dart
+++ lib/ep1247_art_exhibition_app/src/ui/art_detail_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class ArtDetailPage extends StatefulWidget {
   const ArtDetailPage({Key? key}) : super(key: key);
 
@@ -12,7 +11,49 @@ class _ArtDetailPageState extends State<ArtDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.black.withOpacity(0.8),
+                    Colors.black,
+                  ],
+                ),
+              ),
+              padding: EdgeInsets.fromLTRB(24, 64, 24, 0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.arrow_back_outlined),
+                        color: Colors.white,
+                      ),
+                      Expanded(
+                        child: Text(
+                          "Details",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
