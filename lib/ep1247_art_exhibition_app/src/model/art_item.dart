16af3cc423719243d783ca4a69a2e0657012e8ac
Dreamwalker <aristojeff@gmail.com>
2022-08-21 09:09:16 +0900

    Update art_item.dart

---
+++ lib/ep1247_art_exhibition_app/src/model/art_item.dart
@@ -0,0 +1,27 @@
+class ArtItem {
+  String? img;
+  String? title;
+  String? name;
+  String? date;
+  String? like;
+  String? comments;
+  String? distance;
+  String? category;
+  String? dimension;
+  String? aspectRatio;
+  String? description;
+
+  ArtItem({
+    this.img,
+    this.title,
+    this.name,
+    this.date,
+    this.like,
+    this.comments,
+    this.distance,
+    this.category,
+    this.dimension,
+    this.aspectRatio,
+    this.description,
+  });
+}
