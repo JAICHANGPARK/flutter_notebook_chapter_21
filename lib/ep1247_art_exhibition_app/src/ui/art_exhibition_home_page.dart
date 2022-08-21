0c7db9901b156a9946fd516ddd6e8b19662cd0ba
Dreamwalker <aristojeff@gmail.com>
2022-08-21 09:19:32 +0900

    Update art_exhibition_home_page.dart

--- lib/ep1247_art_exhibition_app/src/ui/art_exhibition_home_page.dart
+++ lib/ep1247_art_exhibition_app/src/ui/art_exhibition_home_page.dart
@@ -64,140 +64,156 @@ class _ArtExhibitionHomePageState extends State<ArtExhibitionHomePage> {
                       padding: EdgeInsets.zero,
                       itemCount: artItems.length,
                       itemBuilder: (context, index) {
+                        var item = artItems[index];
                         return GestureDetector(
                           onTap: () {
                             Navigator.of(context).push(
                               MaterialPageRoute(
-                                builder: (context) => ArtDetailPage(),
+                                builder: (context) => ArtDetailPage(artItem: item,),
                               ),
                             );
                           },
-                          child: Container(
-                            height: 340,
-                            margin: EdgeInsets.only(bottom: 16),
-                            child: Stack(
-                              children: [
-                                Positioned(
-                                  left: 12,
-                                  top: 12,
-                                  bottom: 0,
-                                  right: 0,
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      border: Border.all(
-                                        color: Colors.white,
+                          child: Hero(
+                            tag: "item_$index",
+                            child: Container(
+                              height: 340,
+                              margin: EdgeInsets.only(bottom: 16),
+                              child: Stack(
+                                children: [
+                                  Positioned(
+                                    left: 12,
+                                    top: 12,
+                                    bottom: 0,
+                                    right: 0,
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        border: Border.all(
+                                          color: Colors.white,
+                                        ),
                                       ),
                                     ),
                                   ),
-                                ),
-                                Positioned(
-                                  left: 0,
-                                  top: 0,
-                                  bottom: 12,
-                                  right: 12,
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      color: Colors.white,
-                                    ),
-                                    padding: EdgeInsets.all(8),
-                                    child: Column(
-                                      children: [
-                                        Container(
-                                          height: 160,
-                                          decoration: BoxDecoration(
-                                            color: Colors.black,
-                                            image: DecorationImage(
-                                              image: NetworkImage(
-                                                "https://cdn.pixabay.com/photo/2016/09/08/20/22/angel-1655378__340.jpg",
+                                  Positioned(
+                                    left: 0,
+                                    top: 0,
+                                    bottom: 12,
+                                    right: 12,
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                      ),
+                                      padding: EdgeInsets.all(8),
+                                      child: Column(
+                                        children: [
+                                          Container(
+                                            height: 160,
+                                            decoration: BoxDecoration(
+                                              color: Colors.black,
+                                              image: DecorationImage(
+                                                image: NetworkImage(
+                                                  item.img ?? "",
+                                                  // "https://cdn.pixabay.com/photo/2016/09/08/20/22/angel-1655378__340.jpg",
+                                                ),
+                                                fit: BoxFit.cover,
                                               ),
-                                              fit: BoxFit.cover,
                                             ),
                                           ),
-                                        ),
-                                        SizedBox(
-                                          height: 16,
-                                        ),
-                                        Expanded(
-                                          child: Row(
-                                            crossAxisAlignment: CrossAxisAlignment.start,
-                                            children: [
-                                              CircleAvatar(
-                                                radius: 24,
-                                              ),
-                                              SizedBox(
-                                                width: 16,
-                                              ),
-                                              Expanded(
-                                                child: Column(
-                                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                                  children: [
-                                                    Text(
-                                                      "Can you hear me?",
-                                                      style: TextStyle(
-                                                        fontWeight: FontWeight.bold,
-                                                      ),
-                                                    ),
-                                                    Text(
-                                                      "Zack Jarosz",
-                                                      style: TextStyle(fontSize: 12),
-                                                    ),
-                                                    Row(
-                                                      children: [
-                                                        Icon(
-                                                          Icons.thumb_up_alt_outlined,
-                                                          size: 14,
-                                                        ),
-                                                        SizedBox(
-                                                          width: 4,
-                                                        ),
-                                                        Text("123"),
-                                                        SizedBox(
-                                                          width: 12,
-                                                        ),
-                                                        Icon(
-                                                          Icons.chat_bubble_outline_outlined,
-                                                          size: 14,
-                                                        ),
-                                                        SizedBox(
-                                                          width: 4,
-                                                        ),
-                                                        Text("34"),
-                                                        SizedBox(
-                                                          width: 12,
-                                                        ),
-                                                        Icon(
-                                                          Icons.visibility_outlined,
-                                                          size: 14,
-                                                        ),
-                                                        SizedBox(
-                                                          width: 4,
-                                                        ),
-                                                        Text("8.9m"),
-                                                      ],
-                                                    ),
-                                                    SizedBox(
-                                                      height: 8,
-                                                    ),
-                                                    Text(
-                                                      loremIpsum.substring(0, 100),
-                                                      style: TextStyle(
-                                                        fontSize: 12,
-                                                        color: Colors.grey,
-                                                      ),
-                                                    ),
-                                                    Text("Read more"),
-                                                  ],
-                                                ),
-                                              ),
-                                            ],
+                                          SizedBox(
+                                            height: 16,
                                           ),
-                                        )
-                                      ],
+                                          Expanded(
+                                            child: Row(
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              children: [
+                                                CircleAvatar(
+                                                  radius: 24,
+                                                ),
+                                                SizedBox(
+                                                  width: 16,
+                                                ),
+                                                Expanded(
+                                                  child: Column(
+                                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                                    children: [
+                                                      Text(
+                                                        // "Can you hear me?",
+                                                        item.title ?? "",
+                                                        style: TextStyle(
+                                                          fontWeight: FontWeight.bold,
+                                                        ),
+                                                      ),
+                                                      Text(
+                                                        // "Zack Jarosz",
+                                                        item.name ?? "",
+                                                        style: TextStyle(fontSize: 12),
+                                                      ),
+                                                      Row(
+                                                        children: [
+                                                          Icon(
+                                                            Icons.thumb_up_alt_outlined,
+                                                            size: 14,
+                                                          ),
+                                                          SizedBox(
+                                                            width: 4,
+                                                          ),
+                                                          Text(
+                                                            // "123",
+                                                            item.like ?? "",
+                                                          ),
+                                                          SizedBox(
+                                                            width: 12,
+                                                          ),
+                                                          Icon(
+                                                            Icons.chat_bubble_outline_outlined,
+                                                            size: 14,
+                                                          ),
+                                                          SizedBox(
+                                                            width: 4,
+                                                          ),
+                                                          Text(
+                                                            item.comments ?? "34",
+                                                          ),
+                                                          SizedBox(
+                                                            width: 12,
+                                                          ),
+                                                          Icon(
+                                                            Icons.visibility_outlined,
+                                                            size: 14,
+                                                          ),
+                                                          SizedBox(
+                                                            width: 4,
+                                                          ),
+                                                          Text(
+                                                            // "8.9m",
+                                                            item.distance ?? "8.9m",
+                                                          ),
+                                                        ],
+                                                      ),
+                                                      SizedBox(
+                                                        height: 8,
+                                                      ),
+                                                      Text(
+                                                        // loremIpsum.substring(0, 100),
+                                                        item.description ?? "",
+                                                        style: TextStyle(
+                                                          fontSize: 12,
+                                                          color: Colors.grey,
+                                                        ),
+                                                      ),
+                                                      Text("Read more"),
+                                                    ],
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          )
+                                        ],
+                                      ),
                                     ),
-                                  ),
-                                )
-                              ],
+                                  )
+                                ],
+                              ),
                             ),
                           ),
                         );
